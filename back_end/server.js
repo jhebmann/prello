360b8014c5879a0f65e33c606561acacc443f70f
ouassim <wassimbenmosbah@gmail.com>
2017-10-13 15:06:59 +0200

    renaming controllers

--- back_end/server.js
+++ back_end/server.js
@@ -3,26 +3,27 @@ const app = express();
 const server = require('http').createServer(app);  
 const io = require('socket.io')(server);
 const bodyParser = require('body-parser');
-const eventController = require('./controllers/eventController.js')
+const eventController = require('./controllers/event.js')
 const models = require('./models/index')
 
 //Database declaration
 const mongoose = require('mongoose')
 
-const mongodbUri = "mongodb://localhost:27017/test";
+const mongodbUri = "mongodb://localhost:27017/test"
 const options = {
     useMongoClient: true,
     socketTimeoutMS: 0,
     keepAlive: true,
     reconnectTries: 30
-};
-const db = mongoose.connect(mongodbUri, options);
-const listController =  require('./controllers/cardController.js');
-const listModel = models.Lists
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+}
+const db = mongoose.connect(mongodbUri, options)
 
-let numUsers=0;
+const cardController =  require('./controllers/card.js')
+const listModel = models.Lists
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+let numUsers = 0
 
 io.on('connection', (client) => {
     eventController.get_all_cards(client,listModel)
@@ -54,8 +55,8 @@ io.on('connection', (client) => {
 });
 
 //External Routes BackEnd (Testing only for now) 
-app.route('/').get(listController.list_all_cards).post(listController.create_a_card);
-app.route('/delete_collection').delete(listController.delete_collection);
+app.route('/').get(cardController.list_all_cards).post(cardController.create_a_card);
+app.route('/delete_collection').delete(cardController.delete_collection);
 
 const port = 8000;
 server.listen(port);
