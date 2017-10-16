8f6745bf0657e3fd41ddc5bcb4d253914cc1b914
ouassim <wassimbenmosbah@gmail.com>
2017-10-16 17:20:50 +0200

    controllers added

---
+++ back_end/controllers/list.js
@@ -0,0 +1,32 @@
+/*'use strict';
+const mongoose = require('mongoose')
+const models = require('../models/index')
+const lists = models.Lists
+
+exports.findAll = function(req, res) {
+  lists.find({}, function(err, card) {
+    if (err)
+      res.send(err);
+    res.json(card);
+  });
+};
+
+exports.add = function(req, res) {
+  console.log(req.body);  
+  const newList = new lists(req.body);
+  newList.save(function(err, card) {
+    if (err)
+      res.send(err);
+    res.json(card);
+  });
+};
+
+exports.deleteAll = function(req, res) {
+  lists.remove({}, function(err, card) {
+    if (err){
+      console.log('problems')
+      res.send(err);
+    }
+    res.json({ message: 'Collection successfully deleted' });
+  });
+};*/
