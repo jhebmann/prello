72bbfa3bfe072e1821491f093bb6f0ed51101010
ouassim <wassimbenmosbah@gmail.com>
2017-10-17 17:38:27 +0200

    preparing registeration

--- front_end/src/components/Register/Register.js
+++ front_end/src/components/Register/Register.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import {Button} from 'react-bootstrap';
 
 // Not done yet
 class Register extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
     render(){
-        return (<h3>Register</h3>)
+        return (
+            <div>
+                <h1> Signup </h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label>Email
+                            <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input type="password" name="password"/>
+                    </div>
+                    <Button bsStyle="success" onClick={this.onClickAddUser}>Signup</Button>
+                </form>
+            </div>
+        )
     }
 }
 
