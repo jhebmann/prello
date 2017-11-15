c87ef66b8bf488c84fdcd41929396dbf45e5176d
ouassim <wassimbenmosbah@gmail.com>
2017-11-15 18:52:07 +0100

    dnd begining

--- front_end/src/components/Board/Card.js
+++ front_end/src/components/Board/Card.js
@@ -3,7 +3,6 @@ import { Glyphicon, Thumbnail } from 'react-bootstrap'
 import SkyLight from 'react-skylight'
 import Popup from './Popup.js'
 import moment from 'moment'
-import Draggable from 'react-draggable';
 import axios from 'axios'
 import { confirmAlert } from 'react-confirm-alert'
 import url from '../../config'
@@ -61,26 +60,24 @@ class Card extends React.Component{
         
         return(
             <div>
-                <Draggable>
-                    <div>
-                        <Thumbnail onClick={() => this.customDialog.show()}
-                            className={('undefined' !== typeof this.state.parameters && this.state.cardInfos._id === this.state.parameters.cardId) ? "selected card" : "card"}
-                        >
-                            {(this.state.cardInfos.labels && this.state.cardInfos.labels.length > 0) && 
-                                <div>{this.state.cardInfos.labels.length} labels</div>
-                            }
-                            <div id="glyphRemoveCard" onClick={this.onClickDeleteCard}><Glyphicon glyph='remove' id="glyphRemoveCardChild"/></div>
-                            <h4>{this.state.cardInfos.title}</h4>
-                            {(this.state.cardInfos.description || this.state.cardInfos.comments.length > 0 || this.state.cardInfos.dueDate) &&
-                                <div>
-                                    {dueDateDiv}
-                                    <div className="alignElements"><p> {(this.state.cardInfos.description) && <Glyphicon glyph='align-left'/>} </p></div>
-                                    <div>{(this.state.cardInfos.comments && this.state.cardInfos.comments.length > 0) ? <div><Glyphicon glyph='comment'/>{this.state.cardInfos.comments.length}</div> : ''}</div>
-                                </div>
-                            }
-                        </Thumbnail>
-                    </div>
-                </Draggable>
+                <div>
+                    <Thumbnail onClick={() => this.customDialog.show()}
+                        className={('undefined' !== typeof this.state.parameters && this.state.cardInfos._id === this.state.parameters.cardId) ? "selected card" : "card"}
+                    >
+                        {(this.state.cardInfos.labels && this.state.cardInfos.labels.length > 0) && 
+                            <div>{this.state.cardInfos.labels.length} labels</div>
+                        }
+                        <div id="glyphRemoveCard" onClick={this.onClickDeleteCard}><Glyphicon glyph='remove' id="glyphRemoveCardChild"/></div>
+                        <h4>{this.state.cardInfos.title}</h4>
+                        {(this.state.cardInfos.description || this.state.cardInfos.comments.length > 0 || this.state.cardInfos.dueDate) &&
+                            <div>
+                                {dueDateDiv}
+                                <div className="alignElements"><p> {(this.state.cardInfos.description) && <Glyphicon glyph='align-left'/>} </p></div>
+                                <div>{(this.state.cardInfos.comments && this.state.cardInfos.comments.length > 0) ? <div><Glyphicon glyph='comment'/>{this.state.cardInfos.comments.length}</div> : ''}</div>
+                            </div>
+                        }
+                    </Thumbnail>
+                </div>
                
                 <SkyLight dialogStyles = {bigPopup} hideOnOverlayClicked ref = {ref => this.customDialog = ref}>
                     <Popup listTitle = {this.state.listTitle} card = {this} cardInfos = {this.state.cardInfos} attachments = {this.state.attachments} io={this.socket}
--- front_end/src/components/Board/List.js
+++ front_end/src/components/Board/List.js
@@ -3,6 +3,32 @@ import {Button, FormControl, Glyphicon, Panel} from 'react-bootstrap'
 import axios from 'axios'
 import Card from './Card.js'
 import url from '../../config'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
+}
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver && 'lightblue',
+  marginTop: "10px",
+})
+
+const getItemStyle = (draggableStyle, isDragging) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+
+  // change background colour if dragging
+  background: isDragging && 'lightgreen',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+})
 
 class List extends React.Component{
 
@@ -32,6 +58,7 @@ class List extends React.Component{
     this.deleteCard = this.deleteCard.bind(this)
 
     this.handleInputChange = this.handleInputChange.bind(this)
+    this.onDragEnd = this.onDragEnd.bind(this)
 
     //Event Listeners
     this.socket.on('updateListTitle', this.updateListTitle)
@@ -39,6 +66,23 @@ class List extends React.Component{
     this.socket.on('deleteCards', this.deleteCards)
     this.socket.on('deleteCardClient', this.deleteCard)
   
+  }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const newCards = reorder(
+      this.state.cards,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      cards: newCards
+    });
   }
 
   componentDidMount() {
@@ -71,7 +115,19 @@ class List extends React.Component{
             </div>
           </div>
           <div className="listBody">
-            {this.cardList(this.state.cards)} 
+            <DragDropContext onDragEnd={this.onDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    style={getListStyle(snapshot.isDraggingOver)}
+                  >
+                    {this.cardList()}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+
             <p>
               <FormControl type="text" onChange={this.handleInputChange} placeholder="Card Title" name="titleNewCard" 
                             value={this.state.titleNewCard} onKeyPress={this.handleKeyPress}/>
@@ -84,9 +140,24 @@ class List extends React.Component{
   } 
 
   //Renders the Cards stored in the cards array   
-  cardList(list){
-    const cardItems= this.state.cards.map((card, index)=>
-      <Card parameters = {this.state.parameters} boardId={this.props.idBoard} listTitle={this.state.title} listId = {this.props.id} key={card._id} cardInfos={card} io={this.socket} usersBoard={this.props.usersBoard}/>
+  cardList(){
+    const cardItems= this.state.cards.map((card, index) =>
+    <Draggable key={card._id} draggableId={card._id}>
+      {(provided, snapshot) => (
+        <div>
+          <div ref={provided.innerRef}
+            style={getItemStyle(
+              provided.draggableStyle,
+              snapshot.isDragging
+            )}
+            {...provided.dragHandleProps}
+          >
+            <Card parameters = {this.state.parameters} boardId={this.props.idBoard} listTitle={this.state.title} listId = {this.props.id} key={card._id} cardInfos={card} io={this.socket} usersBoard={this.props.usersBoard}/>
+          </div>
+          {provided.placeholder}
+        </div>
+      )}
+    </Draggable>
     )
     return cardItems
   }
