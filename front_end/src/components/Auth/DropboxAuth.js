c3fbd5902885344b2ee5d55907fee32cffd3f1e7
jhebmann <julien.hebmann@gmx.fr>
2017-11-19 22:29:42 +0100

    fixed crash if accessing dropboxauth path

--- front_end/src/components/Auth/DropboxAuth.js
+++ front_end/src/components/Auth/DropboxAuth.js
@@ -5,28 +5,28 @@ import Auth from './Auth'
 class DropboxAuth extends React.Component{
     constructor(props) {
         super(props)
-        
+
         const queryString = require('query-string')
         const parsed = queryString.parse(window.location.hash)
 
-        Auth.setDropboxToken(parsed.access_token)
+        if ('undefined' !== typeof parsed.state) {
+            Auth.setDropboxToken(parsed.access_token)
 
-        this.state = {
-            access_token: parsed.access_token,
-            account_id: parsed.account_id,
-            state: parsed.state,
-            pathname: parsed.state.split('|')[0],
-            cardId: parsed.state.split('|')[1],
-            token_type: parsed.token_type,
-            uid: parsed.uid
+            this.state = {
+                access_token: parsed.access_token,
+                account_id: parsed.account_id,
+                state: parsed.state,
+                pathname: parsed.state.split('|')[0],
+                cardId: parsed.state.split('|')[1],
+                token_type: parsed.token_type,
+                uid: parsed.uid
+            }
         }
-        
-        
-
     }
 
     render() {
-        return <Redirect to={{pathname: this.state.pathname, cardToFocus: this.state.cardId, access_token: this.state.access_token}} />
+        return (this.state) ? <Redirect to={{pathname: this.state.pathname, cardToFocus: this.state.cardId, access_token: this.state.access_token}} />
+                    : <Redirect to="/"/>
     }
 }
 
